0f1aea00d8b751ec0077ab5d2c6a5276bee5663d
Walid Bouladam <bouladam.walid86@gmail.com>
2022-07-20 12:53:32 +1200

    Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const colors = require('colors')
 const { graphqlHTTP } = require('express-graphql')
+
 const schema = require('./schema/schema')
+const connectDB = require('./config/db')
+
 require('dotenv').config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const app = express()
 
+// MongoDB
+connectDB()
+
+// GraphQL
 app.use(
   '/graphql',
   graphqlHTTP({
