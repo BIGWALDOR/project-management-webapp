024024697f26d18434c2ee0f62aeb174e6887360
Walid Bouladam <bouladam.walid86@gmail.com>
2022-07-21 21:55:52 +1200

    Imported Projects component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './index.css'
 import Header from './components/Header'
 import Clients from './components/Clients'
 import AddClientModal from './components/AddClientModal'
+import Projects from './components/Projects'
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -36,6 +37,7 @@ const App = () => {
         <Header />
         <div className='container'>
           <AddClientModal />
+          <Projects />
           <Clients />
         </div>
       </ApolloProvider>
