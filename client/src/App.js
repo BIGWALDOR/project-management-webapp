6cb0bd1b60fdcd71fd0fc8890ceb400c2aaaa6c7
Walid Bouladam <bouladam.walid86@gmail.com>
2022-07-21 21:12:23 +1200

    Implemented read/write gql caching

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,28 @@ import './index.css'
 import Header from './components/Header'
 import Clients from './components/Clients'
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+      },
+    },
+  },
+})
+
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 })
 
 const App = () => {
