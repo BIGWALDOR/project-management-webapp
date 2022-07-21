ca2ceddcd2c93912f26c2cd222575b1a302c058c
Walid Bouladam <bouladam.walid86@gmail.com>
2022-07-21 16:22:28 +1200

    Import ApolloProvider/Client and InMemoryCache

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+
 import './index.css'
 import Header from './components/Header'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
+
 const App = () => {
-  return <Header />
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+      </ApolloProvider>
+    </>
+  )
 }
 
 export default App
