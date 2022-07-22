6bd0d9621b5fd3593a06222a1b5446b107005352
Walid Bouladam <bouladam.walid86@gmail.com>
2022-07-23 11:01:11 +1200

    Added conditional when env is in prod

--- server/index.js
+++ server/index.js
@@ -25,6 +25,14 @@ app.use(
   })
 )
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 app.listen(
   process.env.PORT || 4000,
   console.log(`Server running on port ${process.env.PORT}`)
